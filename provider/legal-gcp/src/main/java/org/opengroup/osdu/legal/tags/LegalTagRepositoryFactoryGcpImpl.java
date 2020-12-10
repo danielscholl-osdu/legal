5e74a8cb0268c6dc1af8fcab70407c909b49f3bf
Anastasiia_Gelmut <anastasiia_gelmut@epam.com>
2020-12-10 13:40:33 +0400

    GONRG-1366 Fixed Sonar Comments

--- provider/legal-gcp/src/main/java/org/opengroup/osdu/legal/tags/LegalTagRepositoryFactoryGcpImpl.java
+++ provider/legal-gcp/src/main/java/org/opengroup/osdu/legal/tags/LegalTagRepositoryFactoryGcpImpl.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2020 Google LLC
+ * Copyright 2020 EPAM Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.opengroup.osdu.legal.tags;
 
 import com.google.cloud.datastore.Datastore;
@@ -46,8 +63,8 @@ public class LegalTagRepositoryFactoryGcpImpl implements ILegalTagRepositoryFact
   }
 
   private void addRepository(String tenantName) {
-    TenantInfo tenantInfo = tenantFactory.getTenantInfo(tenantName);
-    Datastore ds = factory.getDatastore(tenantInfo);
+    TenantInfo info = tenantFactory.getTenantInfo(tenantName);
+    Datastore ds = factory.getDatastore(info);
     if (Objects.isNull(ds)) {
       throw invalidTenantGivenException(tenantName);
     }
