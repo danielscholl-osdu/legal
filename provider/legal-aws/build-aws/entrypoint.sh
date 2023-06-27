4b2afafcf837991338a99f8284b43dae55f4e15e
Rustam_Lotsmanenko <rustam_lotsmanenko@epam.com>
2023-06-27 13:33:38 +0300

    add run for aws args, change source image for gc

--- provider/legal-aws/build-aws/entrypoint.sh
+++ provider/legal-aws/build-aws/entrypoint.sh
@@ -1,1 +1,2 @@
-java $JAVA_OPTS -jar /app.jar
+# --add-opens args used to open modules and allow illegal(reflection\private classes and fields) access from the code.
+java $JAVA_OPTS -jar /app.jar --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED
