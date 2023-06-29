d35255fce9da389e1b930c760b629e5252a65c53
Rustam_Lotsmanenko <rustam_lotsmanenko@epam.com>
2023-06-29 13:33:09 +0300

    move args

--- provider/legal-aws/build-aws/entrypoint.sh
+++ provider/legal-aws/build-aws/entrypoint.sh
@@ -1,2 +1,2 @@
 # --add-opens args used to open modules and allow illegal(reflection\private classes and fields) access from the code.
-java $JAVA_OPTS -jar /app.jar --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED
+java $JAVA_OPTS --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED -jar /app.jar
