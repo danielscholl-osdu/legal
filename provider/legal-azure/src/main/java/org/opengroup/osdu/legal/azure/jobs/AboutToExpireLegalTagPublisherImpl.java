4f9f96942ddba9015d1e0f5abc7f771c04b8c464
Wesley Evans <wesleyevans@microsoft.com>
2024-12-13 18:21:16 +0000

    Implementation of Azure notification publisher for about to expire legal tags

--- provider/legal-azure/src/main/java/org/opengroup/osdu/legal/azure/jobs/AboutToExpireLegalTagPublisherImpl.java
+++ provider/legal-azure/src/main/java/org/opengroup/osdu/legal/azure/jobs/AboutToExpireLegalTagPublisherImpl.java
@@ -7,9 +7,19 @@ import org.opengroup.osdu.legal.jobs.models.AboutToExpireLegalTags;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AboutToExpireLegalTagPublisherImpl implements IAboutToExpireLegalTagPublisher {
+public class AboutToExpireLegalTagPublisherImpl extends AbstractPublisher implements IAboutToExpireLegalTagPublisher {
+    protected final static String LEGAL_TAGS_CHANGE_EVENT_SUBJECT = "legaltagclosetoexpiretopic";
+    protected final static String LEGAL_TAGS_CHANGE_EVENT_TYPE = "legaltagclosetoexpiretopic";
+    protected final static String LEGAL_TAGS_CHANGE_EVENT_DATA_VERSION = "1.0";
+    protected final static String LEGAL_TAGS_CHANGE_EVENT_DEBUG_MESSAGE = "Legal publishes about to expire tag event";
+
+    public AboutToExpireLegalTagPublisherImpl() {
+        super(LEGAL_TAGS_CHANGE_EVENT_SUBJECT, LEGAL_TAGS_CHANGE_EVENT_TYPE, LEGAL_TAGS_CHANGE_EVENT_DATA_VERSION, LEGAL_TAGS_CHANGE_EVENT_DEBUG_MESSAGE);
+    }
+
     @Override
     public void publish(String projectId, DpsHeaders headers, AboutToExpireLegalTags aboutToExpireLegalTags) {
-        // to be implemented
+        publishToServiceBus(headers, aboutToExpireLegalTags);
+        publishToEventGrid(headers, aboutToExpireLegalTags);
     }
 }
